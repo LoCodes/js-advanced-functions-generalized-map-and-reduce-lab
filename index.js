29cd4ca50d4d77f88741936e2dd58abaed07ee8f
Loren Cipriano <loren25cipriano@gmail.com>
2021-06-17 12:53:15 -0700

    commit tree 15f1b9e56fc12de55f27692aa21dd1ee8c43e027

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map (array, callback) {
+    const returnArray = []
+    for (let i = 0; i < array.length; i++) {
+      returnArray.push(callback(array[i]))
+    }
+    return returnArray
+  }
+
+
+  function reduce (array, cb, startValue) {
+    let runningTotal
+    if (startValue) {
+      runningTotal = startValue
+      for (let i = 0; i < array.length; i++) {
+        runningTotal = cb(array[i], runningTotal)
+      }
+    } else {
+      runningTotal = array[0]
+      for (let i = 1; i < array.length; i++) {
+        runningTotal = cb(array[i], runningTotal)
+      }
+    }
+    return runningTotal
+}
